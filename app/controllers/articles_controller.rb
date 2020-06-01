aab76bfc6496aabcf7edee651f82317b15885950
Eduardo Medina <emedina@alset.com.mx>
2020-06-01 13:58:06 -0500

    Add one-to-many user article association

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,63 +1,62 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
 
-    before_action :set_article, only: [:edit, :update, :show, :destroy]
+  def show
+    #byebug
+    #instance variable @article is available generally for the whole controller
+  end
 
-    def show
-        #byebug
-        #instance variable @article is available generally for the whole controller
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  # Action
+  def create
+    #render plain: params[:article] #loads as plain text
+    #render plain: @article.inspect
+
+    #white list, strong params security introduced in RoR 6
+    @article = Article.new(get_article_params)
+    @article.user = User.first
+    if @article.save {
+      flash[:notice] = "Article was created successfully."
+      #redirect to show, it extracts the article id automatically
+      redirect_to article_path(@article) #redirect_to @article
+    }
+    else
+      render "new" #renders the new view again
     end
+  end
 
-    def index
-        @articles = Article.all
-    end
+  def edit
+  end
 
-    def new
-        @article = Article.new
+  # Action
+  def update
+    if @article.update(get_article_params)
+      flash[:notice] = "Article was updated succesfully"
+      redirect_to @article
+    else
+      render "edit"
     end
+  end
 
-    # Action
-    def create
-        #render plain: params[:article] #loads as plain text
-        #render plain: @article.inspect
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
 
-        #white list, strong params security introduced in RoR 6
-        @article = Article.new(get_article_params)
-         
-        if @article.save {
-            flash[:notice] = "Article was created successfully."
-            #redirect to show, it extracts the article id automatically
-            redirect_to article_path(@article) #redirect_to @article
-        }            
-        else
-            render 'new' #renders the new view again
-        end
-    end
+  private
 
-    def edit
-    end
-    
-    # Action
-    def update
-        if @article.update(get_article_params);
-            flash[:notice] = "Article was updated succesfully"
-            redirect_to @article
-        else
-            render 'edit'
-        end
-    end
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
-    def destroy
-        @article.destroy
-        redirect_to  articles_path
-    end
-
-    private 
-    def set_article
-        @article = Article.find(params[:id])
-    end
-    
-    def get_article_params
-        params.require(:article).permit(:title, :description) 
-    end
-
+  def get_article_params
+    params.require(:article).permit(:title, :description)
+  end
 end
