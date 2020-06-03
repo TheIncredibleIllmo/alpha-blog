eac13d2647dd66019f22eb2f979e09c2c874dc36
Eduardo Medina <emedina@alset.com.mx>
2020-06-03 14:55:00 -0500

    user session helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,14 @@ module ApplicationHelper
     gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
     image_tag(gravatar_url, alt: user.username, class: "shadow-lg mx-auto d-block m-4 circle-img")
   end
+
+  def current_user
+    #one option or the other
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    #!! transforms the result in a boolean
+    !!current_user
+  end
 end
