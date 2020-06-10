e1ca99d947180272290e5283eef86733e90dc442
Eduardo Medina <emedina@alset.com.mx>
2020-06-09 19:33:23 -0500

    Category model Unit Tests

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,9 +1,35 @@
 require "test_helper"
 
 class CategoryTest < ActiveSupport::TestCase
-  test "category should be valid" do
+  #runs before any test
+  def setup
     @category = Category.new(name: "Sports")
+  end
+
+  test "category should be valid" do
     #assetion
     assert @category.valid?
+  end
+
+  test "name should be present" do
+    @category.name = ""
+    assert_not @category.valid?
+  end
+
+  test "name should be unique" do
+    #it should be saved to validate uniqueness
+    @category.save
+    @second_category = Category.new(name: "Sports")
+    assert_not @second_category.valid?
+  end
+
+  test "name should not be too long" do
+    @category.name = "a" * 26
+    assert_not @category.valid?
+  end
+
+  test "name should not be too short" do
+    @category.name = "aa"
+    assert_not @category.valid?
   end
 end
